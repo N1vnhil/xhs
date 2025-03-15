1400becbc164eaf2bca7edeaf66b2662aa320bb3
n1nvhil <n1vnhil@outlook.com>
2025-03-15 18:55:39 +0800

    create role constants

--- xhs-auth/src/main/java/org/n1vnhil/xhsauth/constant/RedisKeyConstants.java
+++ xhs-auth/src/main/java/org/n1vnhil/xhsauth/constant/RedisKeyConstants.java
@@ -8,4 +8,22 @@ public class RedisKeyConstants {
         return VERIFICATION_CODE_PREFIX + phone;
     }
 
+    /**
+     * xhs全局id生成redis key
+     */
+    public static final String XHS_ID_GENERATE_KEY = "xhs_id_generator";
+
+    /**
+     * 用户角色前缀
+     */
+    private static final String USER_ROLE_KEY_PREFIX = "user:roles:";
+
+    /**
+     * 根据用户手机号构造用户角色redis key
+     * @param phone 用户手机号
+     * @return 用户角色redis key
+     */
+    private String buildUserRoleKey(String phone) {
+        return USER_ROLE_KEY_PREFIX + phone;
+    }
 }
---
+++ xhs-auth/src/main/java/org/n1vnhil/xhsauth/constant/RoleConstants.java
@@ -0,0 +1,11 @@
+package org.n1vnhil.xhsauth.constant;
+
+
+public class RoleConstants {
+
+    /**
+     * 普通用户角色 id
+     * */
+    public static final Long COMMON_USER_ROLE_ID = 1L;
+
+}
