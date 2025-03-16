8e413b6abe038b60f60c0e83ee477e0a417db7a7
n1nvhil <n1vnhil@outlook.com>
2025-03-16 20:25:22 +0800

    fix nacos register problem

--- xhs-auth/src/main/java/org/n1vnhil/xhsauth/XhsAuthApplication.java
+++ xhs-auth/src/main/java/org/n1vnhil/xhsauth/XhsAuthApplication.java
@@ -3,9 +3,12 @@ package org.n1vnhil.xhsauth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @MapperScan("org.n1vnhil.xhsauth.domain.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class XhsAuthApplication {
 
     public static void main(String[] args) {
