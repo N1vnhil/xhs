0565d41f491f97112f835d5066cea70a0be4059a
n1nvhil <n1vnhil@outlook.com>
2025-03-24 00:36:12 +0800

    create user service

---
+++ xhs-user/xhs-user-biz/src/main/java/org/n1vnhil/xhs/user/biz/XhsUserBizApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsUserBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsUserBizApplication.class, args);
+    }
+
+}
