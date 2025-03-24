47bb2c1b830107113e4bd60c5f4a2900bdf1389e
n1nvhil <n1vnhil@outlook.com>
2025-03-25 00:03:34 +0800

    创建kv存储服务

---
+++ xhs-kv/xhs-kv-biz/src/main/java/org/n1vnhil/xhs/kv/biz/XhsKvBizApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.kv.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsKvBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsKvBizApplication.class, args);
+    }
+
+}
