5580e3b9368bd48011f8f08abf0f499a0b462718
n1nvhil <n1vnhil@outlook.com>
2025-03-26 12:18:27 +0800

    note service config

---
+++ xhs-note/xhs-note-biz/src/main/java/org/n1vnhil/xhs/note/biz/XhsNoteBizApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.note.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsNoteBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsNoteBizApplication.class, args);
+    }
+
+}
