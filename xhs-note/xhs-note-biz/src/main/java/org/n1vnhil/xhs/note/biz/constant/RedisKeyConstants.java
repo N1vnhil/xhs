5eb8d24bd957f8fc7f977742a42a6583d1e492a5
n1nvhil <n1vnhil@outlook.com>
2025-03-27 00:16:07 +0800

    add note service config

---
+++ xhs-note/xhs-note-biz/src/main/java/org/n1vnhil/xhs/note/biz/constant/RedisKeyConstants.java
@@ -0,0 +1,11 @@
+package org.n1vnhil.xhs.note.biz.constant;
+
+public class RedisKeyConstants {
+
+    public static final String NOTE_DETAIL_KEY = "note:detail:";
+
+    public static String buildNoteDetailKey(Long noteId) {
+       return NOTE_DETAIL_KEY + noteId;
+    }
+
+}
