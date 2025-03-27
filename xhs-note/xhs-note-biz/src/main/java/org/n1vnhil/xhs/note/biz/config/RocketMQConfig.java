1fcf2eeec57a18b8b29ceceed9cd41b0e20b48c7
n1nvhil <n1vnhil@outlook.com>
2025-03-27 14:34:12 +0800

    整合消息队列

---
+++ xhs-note/xhs-note-biz/src/main/java/org/n1vnhil/xhs/note/biz/config/RocketMQConfig.java
@@ -0,0 +1,10 @@
+package org.n1vnhil.xhs.note.biz.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RocketMQConfig {
+
+
+
+}
---
+++ xhs-note/xhs-note-biz/src/main/java/org/n1vnhil/xhs/note/biz/constant/MQConstants.java
@@ -0,0 +1,11 @@
+package org.n1vnhil.xhs.note.biz.constant;
+
+
+public interface MQConstants {
+
+    /**
+     * 删除本地缓存
+     */
+    String TOPIC_DELETE_NOTE_LOCAL_CACHE ="DeleteLocalCacheTopic";
+
+}
