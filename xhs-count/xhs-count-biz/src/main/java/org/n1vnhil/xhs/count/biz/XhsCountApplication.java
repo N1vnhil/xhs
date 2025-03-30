9a94606d74ee2c352f37a13a86e20d5da096960f
n1nvhil <n1vnhil@outlook.com>
2025-03-31 01:36:31 +0800

    创建计数服务

---
+++ xhs-count/xhs-count-biz/src/main/java/org/n1vnhil/xhs/count/biz/XhsCountApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.count.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsCountApplication.class, args);
+    }
+
+}
