8537ab44e138e0694d29fa289fb54711d9c804db
n1vnhil <n1vnhil@outlook.com>
2025-05-14 01:41:35 +0800

    添加数据对齐服务

---
+++ xhs-data-align/src/main/java/org/n1vnhil/xhs/data/align/XhsDataAlignApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.data.align;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsDataAlignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsDataAlignApplication.class, args);
+    }
+
+}
