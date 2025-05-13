246a69a78486646c47987dbc9c8bb24e22c61486
n1vnhil <n1vnhil@outlook.com>
2025-05-14 03:03:42 +0800

    数据对齐服务添加rocketMQ配置

---
+++ xhs-data-align/src/main/java/org/n1vnhil/xhs/data/align/config/RocketMQConfig.java
@@ -0,0 +1,10 @@
+package org.n1vnhil.xhs.data.align.config;
+
+import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(RocketMQAutoConfiguration.class)
+public class RocketMQConfig {
+}
