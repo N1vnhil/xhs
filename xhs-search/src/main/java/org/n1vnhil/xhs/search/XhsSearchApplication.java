ec83352d5af93c6a0dbda788a00eddb97d0a7a70
n1nvhil <n1vnhil@outlook.com>
2025-05-24 01:12:12 +0800

    创建搜索微服务

---
+++ xhs-search/src/main/java/org/n1vnhil/xhs/search/XhsSearchApplication.java
@@ -0,0 +1,13 @@
+package org.n1vnhil.xhs.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhsSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhsSearchApplication.class, args);
+    }
+
+}
